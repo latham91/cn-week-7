0290b7be1fe2aadd08497e33ac35073eaa61cfec
Aaron Latham <latham91@icloud.com>
2024-02-05 16:17:58 +0000

    enhancement: add static route for portfolio

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use("/example", express.static("example"));
+app.use("/portfolio", express.static("portfolio"));
 
 app.listen(5001, () => {
     console.log("Server is running on port 5001");
