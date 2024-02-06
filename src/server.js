855adf6a776047ca719d3039711287f13f006dfc
Aaron Latham <latham91@icloud.com>
2024-02-06 11:12:25 +0000

    chore: cleanup

--- src/server.js
+++ src/server.js
@@ -2,9 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.use("/example", express.static("example"));
-app.use("/portfolio", express.static("portfolio"));
-
 app.listen(5001, () => {
     console.log("Server is running on port 5001");
 });
