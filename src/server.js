2307e7433d70b2c728eda7080eea4ee1f21e130c
Aaron Latham <latham91@icloud.com>
2024-02-06 14:51:02 +0000

    enhancement: added extra validation and try catch for routes

--- src/server.js
+++ src/server.js
@@ -20,69 +20,103 @@ let bookData = [
 // Get All Books
 // Route: GET /books
 app.get("/books", (req, res) => {
-    res.status(200).json({ success: true, data: bookData });
+    return res.status(200).json({ success: true, data: bookData });
 });
 
 // Add a new book
 // Route: POST /books
 app.post("/books", (req, res) => {
-    if (!req.body.name) {
-        res.status(400).json({ success: false, message: "Name is required" });
+    const { name, author } = req.body;
+
+    try {
+        if (!name) {
+            return res.status(400).json({ success: false, message: "Please provide a name" });
+        }
+
+        if (!author) {
+            return res.status(400).json({ success: false, message: "Please provide an author" });
+        }
+
+        const newBook = {
+            id: bookData.length + 1,
+            name,
+            author,
+        };
+
+        bookData.push(newBook);
+
+        return res.status(201).json({ success: true, data: newBook });
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message });
     }
-
-    const newBook = {
-        id: bookData.length + 1,
-        name,
-    };
-
-    bookData.push(newBook);
-
-    res.status(201).json({ success: true, data: newBook });
 });
 
 // Get book by id
 // Route: GET /books/:id
 app.get("/books/:id", (req, res) => {
     const id = req.params.id;
-    const book = bookData.find((book) => book.id === Number(id));
 
-    if (!book) {
-        res.status(404).json({ success: false, message: "Book not found" });
+    try {
+        if (!id) {
+            return res.status(400).json({ success: false, message: "Please provide an id" });
+        }
+
+        const book = bookData.find((book) => book.id === Number(id));
+
+        if (!book) {
+            return res.status(404).json({ success: false, message: "Book not found" });
+        }
+
+        return res.status(200).json({ success: true, data: book });
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message });
     }
-
-    res.status(200).json({ success: true, data: book });
 });
 
 // Delete book by id
 // Route: DELETE /books/:id
 app.delete("/books/:id", (req, res) => {
     const id = req.params.id;
-    const book = bookData.find((book) => book.id === Number(id));
 
-    if (!book) {
-        res.status(404).json({ success: false, message: "Book not found" });
+    try {
+        if (!id) {
+            return res.status(400).json({ success: false, message: "Please provide an id" });
+        }
+
+        const book = bookData.find((book) => book.id === Number(id));
+
+        if (!book) {
+            return res.status(404).json({ success: false, message: "Book not found" });
+        }
+
+        bookData = bookData.filter((book) => book.id !== Number(id));
+
+        return res.status(200).json({ success: true, data: [] });
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message });
     }
-
-    bookData = bookData.filter((book) => book.id !== Number(id));
-
-    res.status(200).json({ success: true, data: bookData });
 });
 
 // Update book by id
 // Route: PATCH /books/:id
 app.patch("/books/:id", (req, res) => {
     const id = req.params.id;
+
+    if (!id) {
+        return res.status(400).json({ success: false, message: "Please provide an id" });
+    }
+
     const book = bookData.find((book) => book.id === Number(id));
 
     if (!book) {
-        res.status(404).json({ success: false, message: "Book not found" });
+        return res.status(404).json({ success: false, message: "Book not found" });
     }
 
     let updatedBook = { ...book, ...req.body };
 
     bookData = bookData.map((book) => (book.id === Number(id) ? updatedBook : book));
 
-    res.status(200).json({ success: true, data: updatedBook });
+    return res.status(200).json({ success: true, data: updatedBook });
 });
 
 app.listen(5001, () => {
