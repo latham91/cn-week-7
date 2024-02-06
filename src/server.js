a67bd6df21df75d096e194e1fb49ca2a1a8ee99f
Aaron Latham <latham91@icloud.com>
2024-02-06 12:38:18 +0000

    enhancement: add get and post route for books

--- src/server.js
+++ src/server.js
@@ -2,6 +2,38 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const bookData = [
+    {
+        id: 1,
+        name: "The Great Gatsby",
+    },
+    {
+        id: 2,
+        name: "To Kill a Mockingbird",
+    },
+];
+
+app.get("/books", (req, res) => {
+    res.status(200).json({ success: true, data: bookData });
+});
+
+app.post("/books", (req, res) => {
+    if (!req.body.name) {
+        res.status(400).json({ success: false, message: "Name is required" });
+    }
+
+    const newBook = {
+        id: bookData.length + 1,
+        name,
+    };
+
+    bookData.push(newBook);
+
+    res.status(201).json({ success: true, data: newBook });
+});
+
 app.listen(5001, () => {
     console.log("Server is running on port 5001");
 });
